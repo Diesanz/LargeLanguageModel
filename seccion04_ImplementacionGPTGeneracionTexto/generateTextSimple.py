68f2b806d479653d80deee099461825bdc437da3
Diesanz <diego.sanzr2003@gmail.com>
2025-10-18 20:28:21 +0200

    (add) implentación estrategia de decodificación basadas en la temepratura y en top_k

--- seccion04_ImplementacionGPTGeneracionTexto/generateTextSimple.py
+++ seccion04_ImplementacionGPTGeneracionTexto/generateTextSimple.py
@@ -2,7 +2,8 @@ import torch
 import torch.nn as nn
 def generate_text_simple(model, idx, max_new_tokens, context_size): #idx  es  una  matriz  (lote,  n_tokens)  de  índices  en  el  contexto  actual 
     for _ in range(max_new_tokens):
-        idx_cond = idx[:, -context_size:]                         #Recortar el contexto  actual  si  excede  el  tamaño  de  contexto  admitido 
+        idx_cond = idx[:, -context_size:]                         #Recortar el contexto  actual  si  excede  el  tamaño  de  contexto  admitido
+                                                                #ejemplo si context_size = 4 : el modelo solo puede ver los últimos 4 tokens para hacer su predicción
         with torch.no_grad():
             logits = model(idx_cond)
         logits = logits[:, -1, :]                                 #Enfocarse solo en el último paso de tiempi de modo que pase (batch, n_token, vocab_size) a (batch, vocab_size)
