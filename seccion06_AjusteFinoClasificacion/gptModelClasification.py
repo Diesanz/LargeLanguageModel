c0348fd572bf31aeb427b259ebce676a8a71b046
Diesanz <diego.sanzr2003@gmail.com>
2025-10-31 17:53:36 +0100

    (add) remodelación del modelo para taraeas de clasificación

---
+++ seccion06_AjusteFinoClasificacion/gptModelClasification.py
@@ -0,0 +1,45 @@
+from seccion05_PreentrenamientoDatosNoEtiquetados.gpt_download import download_and_load_gpt2
+from seccion05_PreentrenamientoDatosNoEtiquetados.loadWeightsGPTModels import load_weights_into_gpt
+from seccion04_ImplementacionGPTGeneracionTexto.gptModel import GPTModel
+import torch
+
+#Reeutilizar configuraciones
+CHOOSE_MODEL = "gpt2-small (124M)"
+BASE_CONFIG = {
+    "vocab_size": 50257,     # Vocabulary size
+    "context_length": 1024,  # Context length
+    "drop_rate": 0.0,        # Dropout rate
+    "qkv_bias": True         # Query-key-value bias
+}
+model_configs = {
+    "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
+    "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+    "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+    "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
+}
+
+def return_clasification_model():
+
+    BASE_CONFIG.update(model_configs[CHOOSE_MODEL])
+    model_size = CHOOSE_MODEL.split(" ")[-1].lstrip("(").rstrip(")")
+    settings, params = download_and_load_gpt2(model_size=model_size, models_dir="gpt2")
+    model = GPTModel(BASE_CONFIG)
+    load_weights_into_gpt(model, params)
+
+    for param in model.parameters():
+        param.requires_grad = False
+
+    torch.manual_seed(123)
+    num_classes = 2
+    model.out_head = torch.nn.Linear(
+        in_features=BASE_CONFIG["emb_dim"],
+        out_features=num_classes
+    )
+
+    for param in model.trf_blocks[-1].parameters():
+        param.requires_grad = True
+
+    for param in model.final_norm.parameters():
+        param.requires_grad = True
+
+    return model
